9ee6af81acbce967233ca5b96dfb53b95d17fdf7
alisiddiqua <aasiddiqua2@gmail.com>
2026-03-25 17:39:04 +0400

    Delete solitary-spicule directory

--- solitary-spicule/lib/core/constants.dart
+++
@@ -1,6 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = 'https://english.fikrokhabar.com/wp-json/wp/v2';
-  static const String postsEndpoint = '/posts';
-  static const String categoriesEndpoint = '/categories';
-  static const String mediaEndpoint = '/media';
-}
