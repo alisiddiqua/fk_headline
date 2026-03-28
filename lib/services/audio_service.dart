f55db0b73ca35d928ab1088b9e946d39b8611b7b
alisiddiqua <aasiddiqua2@gmail.com>
2026-03-28 16:49:51 +0400

    Add files via upload

--- lib/services/audio_service.dart
+++ lib/services/audio_service.dart
@@ -4,6 +4,8 @@ import 'package:just_audio/just_audio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../providers/islamiafkaar_provider.dart';
 
+enum PlaybackMode { normal, loop, shuffle }
+
 // ─── Background Audio Handler ────────────────────────────────────────────────
 class MyAudioHandler extends BaseAudioHandler with SeekHandler {
   final _player = AudioPlayer();
